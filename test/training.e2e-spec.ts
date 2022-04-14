7c16da67d1d113af52036cfd8ca8a74232ad8b99
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-04-14 14:01:06 +0200

    adding async/await

--- test/training.e2e-spec.ts
+++ test/training.e2e-spec.ts
@@ -95,8 +95,8 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  afterAll(() => {
-    app.close();
+  afterAll(async () => {
+    await app.close();
   });
 
   it('/training (POST)', async () => {
