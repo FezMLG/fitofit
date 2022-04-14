bc2bf9c9144eb51d9d539df8a7423d2c36015765
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-04-14 14:00:15 +0200

    adding database url istead of cridentials

--- src/env.validation.ts
+++ src/env.validation.ts
@@ -1,10 +1,6 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
-  DB_HOST: Joi.string().default('localhost'),
-  DB_PORT: Joi.number().default(5432),
-  POSTGRES_PASSWORD: Joi.string().required(),
-  POSTGRES_USER: Joi.string().required(),
-  POSTGRES_DB: Joi.string().required(),
+  DATABASE_URL: Joi.string().required(),
   API_PORT: Joi.number().default(3000),
 });
