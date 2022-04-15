fa659e90a1a277253e873378d83bd7d0d39ffdb5
FezMLG <46359181+FezMLG@users.noreply.github.com>
2022-04-15 21:09:52 +0200

    adding database for tests

--- test/jest-env.ts
+++ test/jest-env.ts
@@ -1,8 +1,9 @@
-// const testEnv = {
-//   NODE_ENV: 'test',
-//   PORT: 3000,
-//   API_PREFIX: 'api',
-//   DATABASE_URL: 'postgres://admin:admin@localhost:5432/fitofit_test',
-// };
+const testEnv = {
+  NODE_ENV: 'test',
+  PORT: 3000,
+  API_PREFIX: 'api',
+  DATABASE_URL: 'postgres://admin:admin@localhost:54321/fitofit_test',
+  DATABASE_LOGGING: false,
+};
 
-// process.env = Object.assign(process.env, testEnv);
+process.env = Object.assign(process.env, testEnv);
